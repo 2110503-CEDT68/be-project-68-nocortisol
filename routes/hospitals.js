7f43e9331d1e5224e643f8f98565676a27424ec2
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-02-24 17:21:22 +0700

    Add assignment 7 files as base code

---
+++ routes/hospitals.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {
+	getHospitals,
+	getHospital,
+	createHospital,
+	updateHospital,
+	deleteHospital,
+} = require("../controllers/hospitals");
+const appointmentRouter = require("./appointment");
+const { protect, authorize } = require("../middleware/auth");
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use("/:hospitalId/appointments", appointmentRouter);
+
+router
+	.route("/")
+	.get(getHospitals)
+	.post(protect, authorize("admin"), createHospital);
+router
+	.route("/:id")
+	.get(getHospital)
+	.put(protect, authorize("admin"), updateHospital)
+	.delete(protect, authorize("admin"), deleteHospital);
+
+module.exports = router;
