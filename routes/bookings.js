d5db8828560282e8429d08ef0070dc6a8b2f1d3d
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-03-02 19:35:26 +0700

    Remove uncessary authorizations

--- routes/bookings.js
+++ routes/bookings.js
@@ -10,15 +10,12 @@ const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
-router
-	.route("/")
-	.get(protect, authorize("admin", "user"), getBookings)
-	.post(protect, addBooking);
+router.route("/").get(protect, getBookings).post(protect, addBooking);
 
 router
 	.route("/:id")
 	.get(protect, getBooking)
-	.put(protect, authorize("admin", "user"), updateBooking)
-	.delete(protect, authorize("admin", "user"), deleteBooking);
+	.put(protect, updateBooking)
+	.delete(protect, deleteBooking);
 
 module.exports = router;
